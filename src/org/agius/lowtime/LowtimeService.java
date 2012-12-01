c36281d5ce0a01ed43913bd259e14d2d15b5f0a0
Mike Croteau <croteau.mike+git@gmail.com>
2012-12-01 18:36:23 -0500

    integrating mij's code into v2.1

---
+++ src/org/agius/lowtime/LowtimeService.java
@@ -0,0 +1,30 @@
+package org.agius.lowtime;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class LowtimeService extends Service {
+
+	private static String TAG = "Lowtime";
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        Log.d(TAG, "LowtimeService started");
+    }
+    
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "LowtimeService destroyed");
+    }
+    
+}
